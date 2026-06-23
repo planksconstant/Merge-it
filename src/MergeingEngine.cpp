fe0173a5ea2afe4787d4acbdcc58c221b1a44bbd
planksconstant <abhishekdharanisha@icloud.com>
2026-06-23 23:41:26 +0530

    feat:multiple files can be merged with a global mergeit call

--- src/MergeingEngine.cpp
+++ src/MergeingEngine.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <qpdf/QPDF.hh>
 #include <qpdf/QPDFPageDocumentHelper.hh>
+#include <qpdf/QPDFWriter.hh>
 #include <string>
 #include <vector>
 
+
+void append_pages(QPDF& main_pdf, const char* input_filename) {
+    try {
+        QPDF next_pdf;
+        next_pdf.processFile(input_filename);
+
+        QPDFPageDocumentHelper main_helper(main_pdf);
+        QPDFPageDocumentHelper next_helper(next_pdf);
+
+        std::vector<QPDFPageObjectHelper> next_pages = next_helper.getAllPages();
+
+        for (auto& page : next_pages) {
+            main_helper.addPage(page, false); // Append to the end of main_pdf in memory
+        }
+        std::cout << "Accumulated: " << input_filename << "\n";
+    } catch (std::exception& e) {
+        std::cerr << "Error processing " << input_filename << ": " << e.what() << "\n";
+    }
+}
 int main(int argc, char *argv[]) {
   if (argc < 4) {
     std::cerr << "During Compilation use this format -> " << "<File1.pdf> "
@@ -12,34 +32,25 @@ int main(int argc, char *argv[]) {
                                          // non existing files and stuff
     return 1;
   }
-  std::string f1 = argv[1];
-  std::string f2 = argv[2];
-  std::string out = argv[3];
+  char* output_file = argv[argc - 1];
+  QPDF main_pdf;
+  main_pdf.processFile(argv[1]);
 
-  try {
-    QPDF pdf1;
-    pdf1.processFile(f1.c_str());
-
-    QPDF pdf2;
-    pdf2.processFile(f2.c_str());
-
-    QPDFPageDocumentHelper dh1(pdf1);
-    QPDFPageDocumentHelper dh2(pdf2);
-
-    std::vector<QPDFPageObjectHelper> pages2 = dh2.getAllPages();
-
-    for (auto &page : pages2) {
-      dh1.addPage(page, false);
-    }
-    QPDFWriter writer(pdf1, out.c_str());
-    writer.setStaticID(true);
-    writer.write();
-
-    std::cout << "Merged \n";
-
-  } catch (std::exception e) {
-    std::cerr << "Cooked\n";
-    return 1;
+  for (int i = 2; i < argc - 1; i++) {
+      append_pages(main_pdf, argv[i]);
   }
+  try {
+        std::cout << "Writing finalized merged file to: " << output_file << "\n";
+        QPDFWriter writer(main_pdf, output_file);
+        writer.setStaticID(true);
+        writer.write();
+        std::cout << "Success!\n";
+    } catch (std::exception& e){
+        std::cerr << "Failed to write output file: " << e.what() << "\n";
+        return 1;
+    }
+  
+
+
   return 0;
 }
